543322339a9f37d0be89eaa02e2d137201950460
Mohamad Supangat <hantamkoding@gmail.com>
2023-10-20 08:59:00 +0700

    split bot function to make multiple callable

---
+++ src/functions.js
@@ -0,0 +1,40 @@
+import * as bf from "./helpers.js";
+import config from "./config.js";
+
+const binance = bf.binanceClient();
+
+export async function tradingViewTA() {
+    let intervals = [];
+    for (const interval of ["5m", "15m", "1h", "4h"]) {
+        let signals = [interval];
+        const ta = await bf.getTradingViewTA(interval);
+
+        for (const signal of ["oscillators", "moving_averages", "summary"]) {
+            signals.push(ta[signal]["RECOMMENDATION"]);
+        }
+        intervals.push(signals);
+    }
+
+    const table = bf
+        .genTable(intervals)
+        .setHeading("int", "OSCI", "MA", "#")
+        .setStyle("ascii-table")
+        .toString();
+    // console.log(table);
+    return `<b>Trading View TA Recommendation</b>\n${config.symbol}\n\n<pre>${table}</pre>`;
+}
+
+export async function getStatus() {
+    const balance = await bf.getBalance(binance);
+    const currentPrice = await bf.currentPrice(binance);
+    const position = await bf.getPosition(binance);
+
+    let text = `balance : <b>${balance}</b>\n\n`;
+    text += `<b>Market ${config.market}</b>\nprice : <b>${currentPrice}</b>`;
+
+    if (position) {
+        text += `position : ${position.type} - ${position.qty}\nentry : ${position.entryPrice}\nmark : ${position.markPrice}\nliquidation : ${position.liquidationPrice}\npnl : <b>${position.pnl}</b>`;
+    }
+
+    return text;
+}
--- src/telegram.js
+++ src/telegram.js
@@ -1,70 +1,31 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import config from "./config.js";
-import * as bf from "./helpers.js";
+import * as fn from "./functions.js";
 
 const bot = new Telegraf(config.telegram_bot_token);
-const binance = bf.binanceClient();
 
 bot.start((ctx) => ctx.reply("Hallloooo"));
-/* bot.help((ctx) => ctx.reply("Send me a sticker")); */
 
 function checkUser(ctx) {
     if (ctx.update.message.from.id !== Number(config.telegram_chat_id)) {
         ctx.reply("WHO ARE YOUUUUUU  ??");
-        return false;
+        return;
     }
-
     return true;
 }
 
 /** mengirimkan status saat ini */
 bot.command("status", async (ctx) => {
-    if (!checkUser(ctx)) return true;
-
-    const balance = await bf.getBalance(binance);
-    const currentPrice = await bf.currentPrice(binance);
-    const position = await bf.getPosition(binance);
-
-    ctx.replyWithHTML(`balance : <b>${balance}</b>`);
-    ctx.replyWithHTML(
-        `<b>Market ${config.market}</b>\n\ncurrent price : <b>${currentPrice}</b>`,
-    );
-
-    if (position) {
-        ctx.replyWithHTML(
-            `position : ${position.type} - ${position.qty}\nentry : ${position.entryPrice}\nmark : ${position.markPrice}\nliquidation : ${position.liquidationPrice}\npnl : <b>${position.pnl}</b>`,
-        );
-    }
+    if (!checkUser(ctx)) return;
+    const status = await fn.getStatus();
+    ctx.replyWithHTML(status);
 });
 
 bot.command("tradingview_ta", async (ctx) => {
-    if (!checkUser(ctx)) return true;
-
-    let intervals = [];
-    for (const interval of ["5m", "15m", "1h", "4h"]) {
-        let signals = [interval];
-        const ta = await bf.getTradingViewTA(interval);
-
-        for (const signal of ["oscillators", "moving_averages", "summary"]) {
-            signals.push(ta[signal]["RECOMMENDATION"]);
-        }
-
-        // console.log(signals);
-
-        intervals.push(signals);
-    }
-
-    console.log(intervals);
-
-    const table = bf
-        .genTable(intervals)
-        .setHeading("OSCI", "MA", "#")
-        .toString();
-    // console.log(table);
-    const text = `<b>Trading View TA Recommendation</b>\n${config.symbol}\n\n<pre>${table}</pre>`;
-    // console.log(table.toString());
-    ctx.replyWithHTML(text);
+    if (!checkUser(ctx)) return;
+    const ta = await fn.tradingViewTA();
+    ctx.replyWithHTML(ta);
 });
 
 bot.launch();
